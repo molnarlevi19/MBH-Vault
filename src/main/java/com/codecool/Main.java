8e4b21d1d43b53aae3bc0ec491cbb03125c6eb9a
Molnár Levente <112870372+molnarlevi19@users.noreply.github.com>
2023-12-07 13:46:33 +0100

    create a password checker class

--- src/main/java/com/codecool/Main.java
+++ src/main/java/com/codecool/Main.java
@@ -1,7 +1,24 @@
 package com.codecool;
 
+import com.codecool.logger.Logger;
+import com.codecool.ui.ConsoleLogger;
+
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
+        Logger logger1 = new ConsoleLogger();
 
+        String filePath = "src/main/resources/input.txt";
+
+        ReadFile readFile = new ReadFile(filePath, logger1);
+        List<String> asd = readFile.readDocumentContent();
+
+        PasswordChecker passwordChecker = new PasswordChecker(asd);
+        List<String> correctPassword = passwordChecker.passwordChecker();
+        logger1.logInfo("The correct password(s): " + correctPassword.toString());
+
+        logger1.logInfo("The number of correct password(s): " + correctPassword.size());
     }
+
 }
